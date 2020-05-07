2d082e9a3325f7ea5dd1181075fe301e529dbedd
Kim <Hughes>
2020-05-07 10:20:44 +0800

    Added input validation, minor styling changes

--- js/app.js
+++ js/app.js
@@ -4,15 +4,17 @@ document.getElementById('trove_form').addEventListener('submit', loadTrove);
 function loadTrove (e) {
   e.preventDefault();
 
+  // Validate that query input field is not empty
+  let form = document.querySelector('.needs-validation');
+  if (form.checkValidity() === false) {
+    form.classList.add('was-validated');
+  }
+
   // Read the values from the form and create the variables
   const authToken = 'mda3k523k482n4rm';
   const query = document.getElementById('search_query').value;
   const zone = document.getElementById('zone').value;
   const qty = document.getElementById('quantity').value;
-  console.log(authToken);
-  console.log(query);
-  console.log(zone);
-  console.log(qty);
 
   // Build the URL
   let url = 'https://api.trove.nla.gov.au/v2/result?';
@@ -32,7 +34,6 @@ function loadTrove (e) {
   }
   // Append number of resutls to url
   url += `&n=${qty}`;
-  console.log(url);
 
   axios
     .get(url, {
@@ -41,21 +42,15 @@ function loadTrove (e) {
     .then((trove) => showOutputBooks(trove));
 
   function showOutputBooks (trove) {
-    //   let books = trove.data.response;
     let books = trove.data.response.zone;
-    console.log(books);
     let result = document.getElementById('result');
-    let title = books[0].records.work[0].title;
-    console.log(title);
-    let troveUrl = books[0].records.work[0].troveUrl;
-    console.log(troveUrl);
+    // let title = books[0].records.work[0].title;
+    // let troveUrl = books[0].records.work[0].troveUrl;
     let worksList = books[0].records.work;
-    console.log(worksList);
 
     let HTMLTemplate = '';
 
     worksList.forEach((work) => {
-      console.log(work);
       HTMLTemplate += `
         <div class="container">
             <div class="row justify-content-center">
@@ -87,34 +82,16 @@ document
 function loadTroveNews (e) {
   e.preventDefault();
 
+  // Validate that query input field is not empty
+  let form = document.querySelector('.needs-validation');
+  if (form.checkValidity() === false) {
+    form.classList.add('was-validated');
+  }
+
   // Read the values from the form and create the variables
   const authToken = 'mda3k523k482n4rm';
   const query = document.getElementById('search_query_news').value;
-  const zone = document.getElementById('zone').value;
   const qty = document.getElementById('quantity_news').value;
-  console.log(authToken);
-  console.log(query);
-  console.log(zone);
-
-  // Build the URL
-  let url = 'https://api.trove.nla.gov.au/v2/result?';
-  // Append key to url
-  url += `key=${authToken}`;
-  // Append enconding to url
-  url += `&enconding=json`;
-  // Read the zone and append to url
-  if (zone !== '') {
-    url += `&zone=${zone}`;
-  }
-  // Append l-format to url
-  url += `&l-format=Book`;
-  // Read the query and append to url
-  if (query !== '') {
-    url += `&q=${query}`;
-  }
-  // Append number of resutls to url
-  url += `&n=${qty}`;
-  console.log(url);
 
   const api = `https://api.trove.nla.gov.au/v2/result?key=${authToken}&q=${query}&zone=newspaper&&encoding=json&n=${qty}`;
 
@@ -125,25 +102,13 @@ function loadTroveNews (e) {
     .then((trove) => showOutputNews(trove));
 
   function showOutputNews (trove) {
-    //   let books = trove.data.response;
     let books = trove.data.response.zone;
-    console.log(books);
     let result = document.getElementById('result');
-    //   let title = books[0].records.work[0].title;
-    //   console.log(title);
-    // let title = books[0].records.article[0].title.value;
-    // console.log(title);
-    //   let troveUrl = books[0].records.work[0].troveUrl;
-    //   console.log(troveUrl);
     let worksList = books[0].records.article;
-    console.log(worksList);
-    //   let mapImage = books[0].records.articule[0].identifier[0].value;
-    //   console.log(mapImage);
 
     let HTMLTemplateNews = '';
 
     worksList.forEach((work) => {
-      // console.log(work);
       HTMLTemplateNews += `
         <div class="container">
             <div class="row justify-content-center">
